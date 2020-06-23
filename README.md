359c1a3170f8786c141d2fd8ddc6f3d620d1096c
James Ma <james@pop-os.localdomain>
2020-06-23 14:28:55 -0700

    added multiple orders

--- README.md
+++ README.md
@@ -14,57 +14,87 @@ Spun off of [this](https://youtu.be/AGpKm0pdTMM) YouTube tutorial.
 6. Create your `config.py` file:
 
 ```
-keys = {
-	# specific_url only filled out if you have it; otherwise replace with None
-        "specific_url": "<specific_item_link>" || None,
+num_items = <number of items you want>
+keys = [
+	{
+	    # specific_url only filled out if you have it; otherwise replace with None
+            "specific_url": "<specific_item_link>" || None,
 
-	# Keep url the same
-        "url": "https://www.supremenewyork.com/shop/all/",
+	    # Keep url the same
+            "url": "https://www.supremenewyork.com/shop/all/",
 
-	# the number of the item as seen in the layout on the shop/all page (second row starts at 8) 
-        "item_number": <number>,
+	    # the number of the item as seen in the layout on the shop/all page (second row starts at 8) 
+            "item_number": <number>,
 
-`	# only fill out item_type if you have item type, name, and style/color wanted; otherwise replace with None
-        "item_type": "<type>" || None,
-        "item_name": "<name>",
-        "style": "<style>",
+`	    # only fill out item_type if you have item type, name, and style/color wanted; otherwise replace with None
+            "item_type": "<type>" || None,
+            "item_name": "<name>",
+            "style": "<style>",
 
-	# fill these with your own
-        "name": "<name>",
-        "email": "<email>",
-        "phone": <phone>,
-        "address": "<address>",
-        "zip": <zip>,
-        "city": "<city>",
+	    # fill these with your own
+            "name": "<name>",
+            "email": "<email>",
+            "phone": <phone>,
+            "address": "<address>",
+            "zip": <zip>,
+            "city": "<city>",
 	
-	# State number in alphabetical order of state abbrevations; California (CA) is 6
-        "state": <state_number>,
-        "cc_number": <card_number>,
-        "exp_month": <month_number>, # 1 is January, 12 is December
-        "exp_year": <year_number>, # 1 is this year, 11 is this year + 10
-        "cvv": <cvv_number>
-}
+	    # State number in alphabetical order of state abbrevations; California (CA) is 6
+            "state": <state_number>,
+            "cc_number": <card_number>,
+            "exp_month": <month_number>, # 1 is January, 12 is December
+            "exp_year": <year_number>, # 1 is this year, 11 is this year + 10
+            "cvv": <cvv_number>
+	},
+	{
+	  <another key list here following the same format>
+	},
+	{
+	  <create the same amount of key lists as num_items you specified above>
+	}
 ```
 An example `config.py` file for reference:
 ```
-keys = {
-        "specific_url": None,
-        "url": "https://www.supremenewyork.com/shop/all/",
-        "item_number": None,
-        "item_type": "jackets",
-        "item_name": "Raglan Court Jacket",
-        "style": "Olive",
-        "name": "John Doe",
-        "email": "johndoesemail@johndoe.com",
-        "phone": 1234567890,
-        "address": "1010 John Doe Way",
-        "zip": 11111,
-        "city": "Los Angeles",
-        "state": 6, # California
-        "cc_number": 123456789,
-        "exp_month": 1, # January
-        "exp_year": 4, # 2023
-        "cvv": 111
+num_items = 2
+keys = [
+	{
+            "specific_url": None,
+            "url": "https://www.supremenewyork.com/shop/all/",
+            "item_number": None,
+            "item_type": "jackets",
+            "item_name": "Raglan Court Jacket",
+            "style": "Olive",
+            "name": "John Doe",
+            "email": "johndoesemail@johndoe.com",
+            "phone": 1234567890,
+            "address": "1010 John Doe Way",
+            ""zip": 11111,
+            ""city": "Los Angeles",
+            ""state": 6, # California
+            ""cc_number": 123456789,
+            ""exp_month": 1, # January
+            ""exp_year": 4, # 2023
+            ""cvv": 111
+	},
+	{
+            "specific_url": None,
+            "url": "https://www.supremenewyork.com/shop/all/",
+            "item_number": 8, # 8th item on the /shop/all page
+            "item_type": None,
+            "item_name": None,
+            "style": None,
+            "name": "John Doe",
+            "email": "johndoesemail@johndoe.com",
+            "phone": 1234567890,
+            "address": "1010 John Doe Way",
+            ""zip": 11111,
+            ""city": "Los Angeles",
+            ""state": 6, # California
+            ""cc_number": 123456789,
+            ""exp_month": 1, # January
+            ""exp_year": 4, # 2023
+            ""cvv": 111
+	}
 }
 
 ```
--- bot.py
+++ bot.py
@@ -1,40 +1,41 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from config import keys
+from config import keys, num_items
 import time
+import multiprocessing
 
 def timeme(method):
     def wrapper(*args, **kw):
         startTime = int(round(time.time() * 1000))
         result = method(*args, **kw)
         endTime = int(round(time.time() * 1000))
-        print((endTime - startTime)/1000, 's')
+        print('order ran in:', (endTime - startTime)/1000, 's')
         return result
     return wrapper
 
 
 @timeme
-def order():
+def order(index, driver):
     # ordering if you already have the specific url ready
-    if keys['specific_url'] != None:
-        driver.get(keys['specific_url'])
+    if keys[index]['specific_url'] != None:
+        driver.get(keys[index]['specific_url'])
     # ordering from all with item number
-    elif keys['item_type'] == None:
+    elif keys[index]['item_type'] == None:
         # get product url
-        driver.get(keys['url'])
-        driver.find_element_by_xpath('//*[@id="container"]/li[{}]/div/a'.format(keys['item_number'])).click()
+        driver.get(keys[index]['url'])
+        driver.find_element_by_xpath('//*[@id="container"]/li[{}]/div/a'.format(keys[index]['item_number'])).click()
         # for the css transition
         time.sleep(.5)
     # ordering from (item_type) page with item name
     else:
         # get product url
-        url = keys['url'] + keys['item_type']
+        url = keys[index]['url'] + keys[index]['item_type']
         driver.get(url)
         # get product of selected color
-        products = driver.find_elements_by_link_text(keys['item_name'])
+        products = driver.find_elements_by_link_text(keys[index]['item_name'])
         for item_type_link in products:
             try:
-                item_type_link.parent.find_element_by_link_text(keys['style']).click()
+                item_type_link.parent.find_element_by_link_text(keys[index]['style']).click()
                 # for the css transition
                 time.sleep(.5)
                 break
@@ -45,33 +46,33 @@ def order():
     driver.find_element_by_name('commit').click()
 
     # wait for checkout button element to load
-    time.sleep(.1)
+    time.sleep(.4)
     driver.find_element_by_class_name('button.checkout').click()
 
     # fill out checkout screen fields
     
     # name
-    driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(keys['name'])
+    driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(keys[index]['name'])
     # email
-    driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(keys['email'])
+    driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(keys[index]['email'])
     # phone
-    driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(keys['phone'])
+    driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(keys[index]['phone'])
     # address
-    driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys['address'])
+    driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys[index]['address'])
     # zip
-    driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys['zip'])
+    driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys[index]['zip'])
     # city
-    driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys['city'])
+    driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys[index]['city'])
     # state
-    driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[{}]'.format(keys["state"])).click()
+    driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[{}]'.format(keys[index]["state"])).click()
     # credit card number
-    driver.find_element_by_id('rnsnckrn').send_keys(keys['cc_number'])
+    driver.find_element_by_id('rnsnckrn').send_keys(keys[index]['cc_number'])
     # credit card expiration month
-    driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[{}]'.format(keys["exp_month"])).click()
+    driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[{}]'.format(keys[index]["exp_month"])).click()
     # cc expiration year
-    driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[{}]'.format(keys["exp_year"])).click()
+    driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[{}]'.format(keys[index]["exp_year"])).click()
     # CCV
-    driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(keys['cvv'])
+    driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(keys[index]['cvv'])
     # terms and conditions
     driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p[2]/label/div/ins').click()
 
@@ -80,8 +81,10 @@ def order():
     
 
 if __name__ == '__main__':
-    # load chrome
-    driver = webdriver.Chrome('./chromedriver')
+    # load chrome(s)
+    drivers = [webdriver.Chrome('./chromedriver') for driver in range(num_items)]
 
-    # call the function above
-    order()
+    for index, driver in enumerate(drivers):
+        # simultaneously make orders
+        multiprocessing.Process(target=order, args=(index, driver, )).start() 
+
