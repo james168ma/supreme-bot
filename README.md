43c357b3d1881743f06ae1ea98ccd2955552415a
James Ma <james@pop-os.localdomain>
2020-06-22 22:29:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Bot for Supreme
+---
+A bot to cop Supreme items.
+Spun off of [this](https://youtu.be/AGpKm0pdTMM) YouTube tutorial.
+---
+# Setting up:
+
+1. Open up terminal
+2. Clone this repo
+3. Go into the repo
+4. Run: `pip install selenium`
+5. Download your chromedriver from [here](http://chromedriver.chromium.org/downloads)
+6. Create your `config.py` file:
+
+```
+keys = {
+	# specific_url only filled out if you have it; otherwise replace with None
+        "specific_url": "<specific_item_link>" || None,
+
+	# Keep url the same
+        "url": "https://www.supremenewyork.com/shop/all/",
+
+	# the number of the item as seen in the layout on the shop/all page (second row starts at 8) 
+        "item_number": <number>,
+
+`	# only fill out item_type if you have item type, name, and style/color wanted; otherwise replace with None
+        "item_type": "<type>" || None,
+        "item_name": "<name>",
+        "style": "<style>",
+        "name": "<name>",
+        "email": "<email>",
+        "phone": <phone>,
+        "address": "<address>",
+        "zip": <zip>,
+        "city": "<city>",
+	
+	# State number in alphabetical order of state abbrevations; California (CA) is 6
+        "state": <state_number>,
+        "cc_number": <card_number>,
+        "exp_month": <month_number>, # 1 is January, 12 is December
+        "exp_year": <year_number>, # 1 is this year, 10 is this year + 10
+        "cvv": <cvv_number>
+}
+```
+---
+# Run it
+```
+python bot.py
+```
+
---
+++ bot.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from config import keys
+import time
+
+def timeme(method):
+    def wrapper(*args, **kw):
+        startTime = int(round(time.time() * 1000))
+        result = method(*args, **kw)
+        endTime = int(round(time.time() * 1000))
+        print((endTime - startTime)/1000, 's')
+        return result
+    return wrapper
+
+# will cookies improve load time?
+# options = webdriver.ChromeOptions()
+# options.add_argument('user-data-dir=www.supremenewyork.com')
+
+@timeme
+def order():
+    # ordering if you already have the specific url ready
+    if keys['specific_url'] != None:
+        driver.get(keys['specific_url'])
+    # ordering from all with item number
+    elif keys['item_type'] == None:
+        # get product url
+        driver.get(keys['url'])
+        driver.find_element_by_xpath('//*[@id="container"]/li[{}]/div/a'.format(keys['item_number'])).click()
+        # for the css transition
+        time.sleep(.5)
+    # ordering from (item_type) page with item name
+    else:
+        # get product url
+        url = keys['url'] + keys['item_type']
+        driver.get(url)
+        # get product of selected color
+        products = driver.find_elements_by_link_text(keys['item_name'])
+        for item_type_link in products:
+            try:
+                item_type_link.parent.find_element_by_link_text(keys['style']).click()
+                # for the css transition
+                time.sleep(.5)
+                break
+            except NoSuchElementException:
+                continue
+
+    # add to cart
+    driver.find_element_by_name('commit').click()
+
+    # wait for checkout button element to load
+    time.sleep(.1)
+    driver.find_element_by_class_name('button.checkout').click()
+
+    # fill out checkout screen fields
+    
+    # name
+    driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(keys['name'])
+    # email
+    driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(keys['email'])
+    # phone
+    driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(keys['phone'])
+    # address
+    driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys['address'])
+    # zip
+    driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys['zip'])
+    # city
+    driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys['city'])
+    # state
+    driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[{}]'.format(keys["state"])).click()
+    # credit card number
+    driver.find_element_by_id('rnsnckrn').send_keys(keys['cc_number'])
+    # credit card expiration month
+    driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[{}]'.format(keys["exp_month"])).click()
+    # cc expiration year
+    driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[{}]'.format(keys["exp_year"])).click()
+    # CCV
+    driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(keys['cvv'])
+    # terms and conditions
+    driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p[2]/label/div/ins').click()
+
+    # process payment
+    driver.find_element_by_name('commit').click()
+    
+
+if __name__ == '__main__':
+    # load chrome
+    driver = webdriver.Chrome('./chromedriver')
+
+    # call the function above
+    order()
